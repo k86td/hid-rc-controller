fc297ca286c24498400a242b25654fd0e9e14a71
Tristan Lepine <tristanlepine14@gmail.com>
2024-08-31 14:07:55 -0400

    removed old tui app and added pedal data in minimal

--- src/tui/minimal.rs
+++ src/tui/minimal.rs
@@ -6,9 +6,63 @@ use crossterm::{
 };
 use hidapi::HidApi;
 use std::{
+    fmt::Display,
     io::{self, Stdout},
     time::Duration,
 };
+
+// TODO: move this somewhere else
+pub struct SteeringWheelData {
+    steering_angle: u8,
+    gas_pedal: DualPrecisionPedal,
+    brake_pedal: DualPrecisionPedal,
+}
+
+pub struct DualPrecisionPedal {
+    precise: u8,
+    general: u8,
+}
+
+impl From<&[u8]> for DualPrecisionPedal {
+    fn from(value: &[u8]) -> Self {
+        Self {
+            precise: value[0],
+            general: value[1],
+        }
+    }
+}
+
+impl From<[u8; 32]> for SteeringWheelData {
+    fn from(value: [u8; 32]) -> Self {
+        Self {
+            steering_angle: value[2],
+            gas_pedal: value[7..=8].into(),
+            brake_pedal: value[3..=4].into(),
+        }
+    }
+}
+
+impl DualPrecisionPedal {
+    fn convert_dual_precision(precise: u8, general: u8, max_value: usize, invert: bool) -> usize {
+        if invert {
+            ((general as i16 * 255) + precise as i16 - max_value as i16)
+                .abs()
+                .try_into()
+                .unwrap()
+        } else {
+            (general * 255 + precise).into()
+        }
+    }
+}
+
+impl Display for DualPrecisionPedal {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(
+            &DualPrecisionPedal::convert_dual_precision(self.precise, self.general, 1020, true)
+                .to_string(),
+        )
+    }
+}
 
 pub fn app(api: &mut HidApi, stdout: &mut Stdout) -> io::Result<()> {
     if let Some(dev) = api.device_list().next() {
@@ -17,26 +71,33 @@ pub fn app(api: &mut HidApi, stdout: &mut Stdout) -> io::Result<()> {
 
             loop {
                 wheel.read(&mut buf).unwrap();
+                let wheel_data: SteeringWheelData = buf.into();
                 if crossterm::event::poll(Duration::from_nanos(10))? {
                     let event = read()?;
                     if event == Event::Key(KeyCode::Char('q').into()) {
                         break;
-                    } else if Event::Resize {
-                        continue;
                     }
                 } else {
                     queue!(stdout, Clear(terminal::ClearType::FromCursorUp),).unwrap();
 
-                    print_middle_row(&format!("{:?}", terminal::size()), 1, stdout);
-                    print_middle_row(&buf[2].to_string(), 2, stdout);
+                    // print_middle_row(&format!("{:?}", buf), 1, stdout);
+                    print_middle_row(&wheel_data.steering_angle.to_string(), 2, stdout);
                     print_middle_row(
                         &steering_wheel_chart(
-                            buf[2].into(),
+                            wheel_data.steering_angle.into(),
                             (terminal::size().unwrap().1).into(),
                             255,
                             "#",
                         ),
                         3,
+                        stdout,
+                    );
+                    print_middle_row(
+                        &format!(
+                            "gas: {} | brake: {}",
+                            wheel_data.gas_pedal, wheel_data.brake_pedal
+                        ),
+                        4,
                         stdout,
                     );
                 }
