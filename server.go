4da371d32bce8f3d85707440cfe728218b51190c
Tristan Lepine <tristanlepine14@gmail.com>
2025-05-17 23:45:37 -0400

    fix: mapped some inputs

--- server.go
+++ server.go
@@ -81,15 +81,16 @@ func main() {
 	hid.Init()
 	defer hid.Exit()
 
-	// hid.Enumerate(0, 0, func(info *hid.DeviceInfo) error {
-	// 	fmt.Println(info)
-	// 	return nil
-	// })
+	hid.Enumerate(0, 0, func(info *hid.DeviceInfo) error {
+		fmt.Println(info)
+		return nil
+	})
 
 	// this is the steering wheel
 	// dev, err := hid.Open(1103, 46742, "")
 	// dev, err := hid.Open(1118, 2354, "fe:8c:7c:9e:14:69")
 	dev, err := hid.Open(1356, 3302, "14:3a:9a:0b:84:5a") // this is Sony Controller
+
 	if err != nil {
 		log.Fatalf("error while opening device: %v", err)
 	}
@@ -110,9 +111,10 @@ func main() {
 
 	ParseReportDescriptor(b)
 
-	// b = make([]byte, 128)
-	// for {
-	// 	dev.Read(b)
-	// 	fmt.Printf("%08b\n", b)
-	// }
+	b = make([]byte, 128)
+	for {
+		dev.Read(b)
+		fmt.Printf("left: x=%03v y=%03v   right: x=%03v y=%03v   lb=%03v rb=%03v\r",
+			b[1], b[2], b[3], b[4], b[5], b[6])
+	}
 }
